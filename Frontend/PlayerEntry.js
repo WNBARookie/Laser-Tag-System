6493e992448d8f68d50888338977d9e96b46946d
garrett whitehead <34140428+gwhiteheadfayar@users.noreply.github.com>
2023-02-17 10:44:15 -0600

    Consolidated HTML and CSS elements

--- Frontend/PlayerEntry.js
+++
@@ -1,6 +0,0 @@
-const startButton = document.getElementById("start-button");
-
-startButton.addEventListener("click", function() {
-  //here is probably where it should load another screen when play is hit
-  console.log("Button clicked");
-});
