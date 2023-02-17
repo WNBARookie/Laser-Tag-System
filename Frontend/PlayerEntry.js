ba2e7ca610997110480156c278f8e056438df148
garrett whitehead <34140428+gwhiteheadfayar@users.noreply.github.com>
2023-02-16 20:00:58 -0600

    +Player Entry Screen, Need to Consolidate Files

---
+++ Frontend/PlayerEntry.js
@@ -0,0 +1,6 @@
+const startButton = document.getElementById("start-button");
+
+startButton.addEventListener("click", function() {
+  //here is probably where it should load another screen when play is hit
+  console.log("Button clicked");
+});
