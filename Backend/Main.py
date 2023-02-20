cfe951f9cfd7452330febbe4915c6c87c1ddf1ed
daxfulce <dp031@uark.edu>
2023-02-19 22:00:54 -0600

    enpoints added and js to add id and codename to db

--- Backend/Main.py
+++ Backend/Main.py
@@ -1,18 +1,35 @@
+from flask import Flask, jsonify, request
+import flask
+import json
+from flask_cors import CORS
 from supabase import create_client, Client
 
 url: str = "https://olounywkvamwjtupzyht.supabase.co"
 key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sb3VueXdrdmFtd2p0dXB6eWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MzQxNjgsImV4cCI6MTk5MTIxMDE2OH0.GZL0CXt5gg8n0NnsPIfEMc8EX77_aajN8Px4gg4nTJg"
 supabase: Client = create_client(url, key)
 
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods = ['GET', 'POST'])
+def home():
+    if(request.method == 'GET'):
+  
+        data = "hello world"
+        return jsonify({'data': data})
+
+@app.route('/getCodeName/<int:idNum>', methods = ['GET'])
 def getCodeName(idNum):
     response = supabase.table("player").select("codename").eq('id', idNum).execute()
-
+    #return response
     for player in response.data:
-        return (player['codename'])
+         return (player['codename'])
 
+@app.route('/insertPlayer/<int:idNum>/<string:codeName>', methods = ['GET', 'POST'])
 def insertPlayer(idNum, codeName):
     supabase.table("player").insert({"id":idNum, "codename":codeName}).execute()
+    data = str(idNum) + " " + codeName
+    return data
 
-codeName = getCodeName(5678)
-print(codeName)
-#insertPlayer(5678, "bob")
+if __name__ == "__main__":
+    app.run("localhost", 6969)
--- Frontend/Main.js
+++ Frontend/Main.js
@@ -14,3 +14,129 @@ document.addEventListener('DOMContentLoaded', (e) => {
     splash.classList.add('none');
   }, 4000);
 });
+
+// HTTP request
+var xhr = null;
+getXmlHttpRequestObject = function () {
+    if (!xhr) {
+    // create a new XMLHttpRequest object
+        xhr = new XMLHttpRequest();
+    }
+    return xhr;
+};
+
+// function sendDataCallback() {
+//     // check if response is ready or not
+//     if (xhr.readyState == 4 && xhr.status == 201) {
+//         dataDiv = document.getElementsByClassName('input-codename');
+//         // set current data text
+//         dataDiv.innerHTML = xhr.responseText;
+//     }
+// }
+
+function sendData(id, name) {
+    xhr = getXmlHttpRequestObject();
+    //xhr.onreadystatechange = sendDataCallback;
+    // asynchronous requests
+    xhr.open("POST", "http://localhost:6969/insertPlayer/" + id + "/" + name, true);
+    //xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // send request over the network
+    //xhr.send(JSON.stringify({"id": id, "name": name}));
+    xhr.send();
+}
+
+// function getDataCallback() {
+// 	// check if response is ready or not
+//     if (xhr.readyState == 4 && xhr.status == 200) {
+//         //dataDiv = document.getElementById('PLACEHOLDER');
+//         // set current data text
+//         //dataDiv.innerHTML = xhr.responseText;
+//         console.log(xhr.response);
+//         return xhr.response;
+//     }
+// }
+
+function getData(idNum) {
+    var response;
+    let xhr = new XMLHttpRequest();
+    // xhr = getXmlHttpRequestObject();
+    //xhr.onreadystatechange = getDataCallback;
+    // asynchronous requests
+    xhr.open("GET", "http://localhost:6969/getCodeName/" + idNum, true);
+    //xhr.onreadystatechange = getDataCallback();
+    // send request over the network
+    xhr.send();
+    xhr.onload = function(){
+      console.log(xhr.response);
+      response = xhr.response;
+    }
+    return response;
+}
+
+// document.addEventListener("keydown", (event) => {
+//   if (event.keyCode === 9) { 
+//     // add FIX for user pressing enter without a space selected ?
+// 	user_id = document.getElementsByClassName('input-id').value; // FIX
+// 	user_name = document.getElementsByClassName('input-codename').value; // FIX
+	
+//   console.log(getData(1));
+
+//   sendData(5, "bill");
+//   }
+
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+		// add FIX for user pressing enter without a space selected ?
+		var user1_id = document.getElementById('r1-id').value; 
+		var user1_name = document.getElementById('r1-cn').value;
+		var user2_id = document.getElementById('g1-id').value;
+		var user2_name = document.getElementById('g1-cn').value;
+		
+    if(user1_id && user1_name){
+      sendData(user1_id, user1_name);
+    }
+    
+    if(user2_id && user2_name){
+      sendData(user2_id, user2_name);
+    }
+
+		// // an id was just entered for either player
+		// if ((user1_id && !user1_name) || (user2_id && !user2_name)) {
+		// 	if (user1_id && !user1_name) {
+		// 		sendData(user1_id, user1_name);
+		// 		//getData()?
+		// 	}
+		// 	else if (user2_id && !user2_name) {
+		// 		sendData(user2_id.textContent, user2_name.textContent);
+		// 		//getData()?
+		// 	}
+		// }
+		// // a codename was just entered for either player 
+		// else if ((user1_id && user1_name && !user2_id) || (user2_id && user2_name && !user1_id)) {
+		// 	if (user1_id && user1_name && !user2_id)
+		// 		sendData(user1_id.textContent, user1_name.textContent);
+		// 	else if (user2_id && user2_name && !user1_id)
+		// 		sendData(user2_id.textContent, user2_name.textContent);
+		// }
+		// return nothing for all other cases
+		else
+			return;
+	}
+
+	//(ASSUMING USER INPUTS DATA AND PRESSES ENTER ON FIRST SPACE BEFORE SECOND SPACE)
+	// if user presses enter on a blank space 
+	// if ((!user_id && !user_name) || (!user_id && user_name))
+	// 	return;
+	// // if user presses enter after inserting id 
+	// else if (user_id && !user_name)
+	// 	// implies that DB retrieval has not occurred yet, so send data
+	// 	//sendData(user_id, user_name);
+	// 	getData(user_id); //?
+	// 	return;
+	// // if user presses enter after inserting codename
+	// // else if (user_id && user_name)
+	// // 	// implies that DB retrieval has already occurred (and found no match), so just write to it
+	// // 	sendData(user_id, user_name);
+	// // 	return;
+  //}
+});
