32cfd2f311cb8becce08f0d3498fd52f6229082b
Antonio_Capito <77825645+AntonioCapito@users.noreply.github.com>
2023-02-19 20:45:11 -0600

    Update 2

---
+++ Backend/Main.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import flask
+import json
+from flask_cors import CORS
+from supabase import create_client, Client
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=['POST', 'GET'])
+def home():
+    if (request.method == 'GET'):
+        data = "hello world"
+        return jsonify({'data': data})
+    if request.method == 'POST': 
+        received_data = request.get_json()
+        nID = received_data['id']
+        nName = received_data['name']
+        return flask.Response(response=json.dumps(return_data), status=201)
+    
+@app.route('/getCodeName/<int:idNum>', methods = ['GET'])
+def getCodeName(idNum):
+    response = supabase.table("player").select("codename").eq('id', idNum).execute()
+
+    for player in response.data:
+        return (player['codename'])
+
+@app.route('/insertPlayer/<int:idNum>/<string:codeName>', methods = ['GET', 'POST'])
+def insertPlayer(idNum, codeName):
+    supabase.table("player").insert({"id":idNum, "codename":codeName}).execute()
+    data = str(idNum) + " " + codeName
+    return data
+
+if __name__ == "__main__":
+    app.run("localhost", 6969)
+
+def main():
+    url: str = "https://olounywkvamwjtupzyht.supabase.co"
+    key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sb3VueXdrdmFtd2p0dXB6eWh0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzU2MzQxNjgsImV4cCI6MTk5MTIxMDE2OH0.GZL0CXt5gg8n0NnsPIfEMc8EX77_aajN8Px4gg4nTJg"
+    supabase: Client = create_client(url, key)
--- Frontend/Main.js
+++ Frontend/Main.js
@@ -14,3 +14,85 @@ document.addEventListener('DOMContentLoaded', (e) => {
     splash.classList.add('none');
   }, 4000);
 });
+
+// -- HTTP request
+var xhr = null;
+getXmlHttpRequestObject = function () {
+    if (!xhr) {
+    // create a new XMLHttpRequest object
+        xhr = new XMLHttpRequest();
+    }
+    return xhr;
+};
+
+function sendDataCallback(name_txt) {
+    // check if response is ready or not
+    if (xhr.readyState == 4 && xhr.status == 201) {
+		if (name_txt.id == 'r1-cn')
+			dataDiv = document.getElementById('r1-cn');
+		else if (name_txt.id == 'g1-cn')
+			dataDiv = document.getElementById('g1-cn');
+        // set current data text
+        dataDiv.innerHTML = xhr.responseText;
+    }
+}
+
+function sendData(id, name) {
+    xhr = getXmlHttpRequestObject();
+    xhr.onreadystatechange = sendDataCallback(name);
+    // asynchronous requests
+    xhr.open("POST", "http://localhost:6969/", true);
+    xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    // send request over the network
+    xhr.send(JSON.stringify({"id": id.textContent, "name": name.textContent}));
+}
+
+function getDataCallback() {
+// check if response is ready or not
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        dataDiv = document.getElementById('PLACEHOLDER');
+        // set current data text
+        dataDiv.innerHTML = xhr.responseText;
+    }
+}
+
+function getData() {
+    xhr = getXmlHttpRequestObject();
+    xhr.onreadystatechange = getDataCallback;
+    // asynchronous requests
+    xhr.open("GET", "http://localhost:6969/", true);
+    // send request over the network
+    xhr.send();
+	
+}
+
+document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 13) {
+		var user1_id = document.getElementById('r1-id'); 
+		var user1_name = document.getElementById('r1-cn');
+		var user2_id = document.getElementById('g1-id');
+		var user2_name = document.getElementById('g1-cn');
+		
+		// an id was just entered for either player
+		if ((user1_id && !user1_name) || (user2_id && !user2_name)) {
+			if (user1_id && !user1_name) {
+				sendData(user1_id, user1_name);
+				//getData()?
+			}
+			else if (user2_id && !user2_name) {
+				sendData(user2_id.textContent, user2_name.textContent);
+				//getData()?
+			}
+		}
+		// a codename was just entered for either player 
+		else if ((user1_id && user1_name && !user2_id) || (user2_id && user2_name && !user1_id)) {
+			if (user1_id && user1_name && !user2_id)
+				sendData(user1_id.textContent, user1_name.textContent);
+			else if (user2_id && user2_name && !user1_id)
+				sendData(user2_id.textContent, user2_name.textContent);
+		}
+		// return nothing for all other cases
+		else
+			return;
+	}
+});
