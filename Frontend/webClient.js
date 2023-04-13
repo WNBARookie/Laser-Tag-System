edf57009b16dc8981ecc92ed025e3f09db23a765
garrett whitehead <34140428+gwhiteheadfayar@users.noreply.github.com>
2023-04-13 13:42:44 -0500

    current game action data display test

--- Frontend/webClient.js
+++ Frontend/webClient.js
@@ -7,9 +7,22 @@ client.onOpen = function(){
 
 client.onerror = function (error) {
   console.log('WebSocket Error ' + error['data']);
-  };
-  
-  // Log messages from the server
+};
+
+// Log messages from the server
 client.onmessage = function (e) {
-    console.log('Server: ' + e['data']);
-  };
+  console.log('Server: ' + e['data']);
+  const [playerX, playerY] = e.data.split(':');
+  const newLine = document.createElement('span');
+  newLine.innerText = `Player ${playerX} hit Player ${playerY}`;
+  const scoreListing = document.querySelector('.scorelisting');
+  scoreListing.appendChild(newLine);
+
+  // Limit the number of messages displayed
+  const maxMessages = 10;
+  const numMessages = scoreListing.children.length;
+  if (numMessages > maxMessages) {
+    scoreListing.removeChild(scoreListing.children[2]);
+  }
+};
+
